3b39f9bb192ade8f06c0ec613b2759bd577dad24
Alper <alperakbulut@gmail.com>
2020-06-11 01:02:51 +0200

    update clientAngular

--- clientAngular/src/app/app.module.ts
+++ clientAngular/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
